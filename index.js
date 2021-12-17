0570aece95c26ea7df1e6ca0081a37d809fb3ac4
DeVogelRyan <devogelryan2@gmail.com>
2021-12-17 14:03:17 +0100

    added categories: PG,PG13,R

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const pool = require("./config/database");
 const userRouter = require('./categories/categories.router')
 
+
+//This is to fix the CORS errors
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -12,22 +14,64 @@ app.use(function(req, res, next) {
 
 app.use(express.json());
 
-
 app.use('/categories',  userRouter);
 
-app.get('/', (req, res)=> {
-    pool.query(
-            'SELECT * FROM PGanswer UNION SELECT * FROM PGquestion ',
-            (err, rows, field)=> {
-                if(!err){
-                        res.json(rows);
+app.get('/PG', (req, res)=> {
+    // source: https://stackoverflow.com/questions/61289125/how-to-retrive-data-from-multiple-tables-using-node-js-and-mysql
+    pool.query("SELECT * FROM PGAnswers",(err,rows,fields)=>{
+        if(!err){
+            pool.query("SELECT * FROM PGQuestions",(errTarn,rowsTarn,fieldsTarn)=>{
+                if(!errTarn){
+                    res.json({
+                      PG :{
+                      answers : rows,
+                      questions: rowsTarn
+                    }});
                 }
-                else { 
-                    res.send(err);
-                }
-            }
-    )
+            }); 
+      
+        }
+        else console.log(err);
+      });
 })
+
+app.get('/PG13', (req, res)=> {
+    // source: https://stackoverflow.com/questions/61289125/how-to-retrive-data-from-multiple-tables-using-node-js-and-mysql
+    pool.query("SELECT * FROM PG13Answers",(err,rows,fields)=>{
+        if(!err){
+            pool.query("SELECT * FROM PG13Questions",(errTarn,rowsTarn,fieldsTarn)=>{
+                if(!errTarn){
+                    res.json({
+                      PG13 :{
+                      answers : rows,
+                      questions: rowsTarn
+                    }});
+                }
+            }); 
+      
+        }
+        else console.log(err);
+      });
+})
+
+app.get('/R', (req, res)=> {
+    // source: https://stackoverflow.com/questions/61289125/how-to-retrive-data-from-multiple-tables-using-node-js-and-mysql
+    pool.query("SELECT * FROM RAnswers",(err,rows,fields)=>{
+        if(!err){
+            pool.query("SELECT * FROM RQuestions",(errTarn,rowsTarn,fieldsTarn)=>{
+                if(!errTarn){
+                    res.json({
+                      R :{
+                      answers : rows,
+                      questions: rowsTarn
+                    }});
+                }
+            }); 
+        }
+        else console.log(err);
+      });
+})
+
 
 
 app.listen(process.env.APP_PORT, ()=>console.log("Listening on port " + process.env.APP_PORT))
